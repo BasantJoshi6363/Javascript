1f914eaf87c73df924c7d6b657d21ae4f5417c0b
BasantJoshi6363 <120437433+BasantJoshi6363@users.noreply.github.com>
2023-12-09 18:00:50 +0000

    day 2 array

---
+++ Day_2_100/Day2_Array.js
@@ -0,0 +1,76 @@
+//array is the collection of element in programming 
+//declaration of array
+let arr = [6,7,8,9]
+let newArr = Array(1,2,3)
+// console.log(arr);
+// console.log(newArr);
+
+//push pop shift unshift
+// add value in array using push pop
+
+arr.push("hello")
+// console.log(arr); // add item in array 
+
+arr.pop()
+// console.log(arr); //it remove the last element of arr 
+
+arr.unshift("hello") // add value at the starting of array 
+// console.log(arr);
+
+arr.shift() // removes the first element of array 
+// console.log(arr);
+
+//Write a function that takes an array of numbers as a parameter and returns the sum of all the elements in the array.
+
+let sumOfAllELement = (value)=>{
+    let sum = 0
+    for (let i = 0; i < value.length; i++) {
+        sum += value[i];
+        
+    }
+    return sum
+}
+
+// console.log(sumOfAllELement([1,2,3,4,5]));
+
+
+//find the maximum value of array
+
+//sum of all array element 
+
+
+let findSumOfAllElement = (value)=>{
+    let sum = 0
+    for (let i = 0; i <value.length; i++) {
+       sum +=value[i]
+    }
+    return sum
+}
+// console.log(findSumOfAllElement([1,2,3,4,5]));
+
+//find the maximum number in array 
+
+let maximumNumber = (value)=>{
+    let largestNum = 0
+    for (let i = 0; i < value.length; i++) {
+        for (let j = 1; j < value.length; j++) {
+            if(value[i]>value[j]){
+                largestNum = value[i]
+            } 
+            else {
+                largestNum = value[j]
+            }           
+        }
+        
+    }
+    return largestNum
+}
+// console.log(maximumNumber([1,2,3,4,5,6,78]));
+
+
+//reverse array 
+
+let reverseArray = (num)=>{
+    return num
+}
+console.log(reverseArray([1,2,3,4,5,6,7]));
