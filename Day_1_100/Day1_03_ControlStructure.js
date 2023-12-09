ed32c24d8077ee1ac5154a1bf9d5b0e3ae9e1525
BasantJoshi6363 <120437433+BasantJoshi6363@users.noreply.github.com>
2023-12-09 17:20:12 +0000

    control structure

--- Day_1_100/Day1_03_ControlStructure.js
+++ Day_1_100/Day1_03_ControlStructure.js
@@ -1,1 +1,5 @@
 //if else
+
+//basically control structure is construct that alter the flow of program execution.
+// basically if else switch for loop while and do while loop are the control structure
+
