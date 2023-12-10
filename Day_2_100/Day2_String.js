5ccdf8e5ea15bfe997d5bb155d48428ef796090e
BasantJoshi6363 <120437433+BasantJoshi6363@users.noreply.github.com>
2023-12-10 17:05:25 +0000

    string

---
+++ Day_2_100/Day2_String.js
@@ -0,0 +1,40 @@
+//strings is data type which is used to store the letters 
+
+// declaration of string
+
+let name = "Basant "
+
+let anoName = new String("Hello my name is Basant")
+
+
+//change into lower and uppercase
+// console.log(name.toUpperCase());
+
+
+// console.log(anoName.toLowerCase());
+
+
+let newstr = "  vbnjkl;"
+
+// console.log(newstr.trim());//removes the additional spaces 
+
+let arkoString = "thorspiderman"
+// console.log(arkoString.charAt(5));// start with 1 
+
+//show the length of the string
+// console.log(arkoString.length);
+
+
+//join() split() reverse()
+
+let meroName = "basant".split("")
+//split the each word and convert into array
+// console.log(meroName);
+// console.log(meroName.reverse())
+// console.log(meroName.reverse().join(""))
+
+
+
+
+
+
