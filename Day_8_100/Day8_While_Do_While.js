cdb2095679878a32d700905322ada3b4b40435d1
BasantJoshi6363 <120437433+BasantJoshi6363@users.noreply.github.com>
2023-12-16 18:31:29 +0000

    final day 10

--- Day_8_100/Day8_While_Do_While.js
+++ Day_8_100/Day8_While_Do_While.js
@@ -14,9 +14,9 @@
 // }
 
 
-let a = 0
+// let a = 0
 
-while(a<=10){
+// while(a<=10){
     // console.log(a)
     //this statement also gives unlimited output because firsly statement is true that a is less than 10, for every iteration it will true thats why it will run unlimited times.
 
@@ -28,14 +28,35 @@ while(a<=10){
 
     // a= a-1 //here our code runs unlimited times. because it will be negative number
     // it will be never positive and reach at 10.
-}
+// }
 
 //let print the array using while loop
-// let initial = 0
-// let moviesGenre = ['action','drama','romantic','comedy'].length
-// console.log(moviesGenre);
+let initial = 0
+let moviesGenre = ['action','drama','romantic','comedy']
+
 
 // while(initial< moviesGenre.length){
 //     console.log(moviesGenre[initial]);
 //     initial++
 // }
+
+
+
+// do while loop
+
+// Syntax
+// let num = 1
+// do {
+//     console.log(num);
+//     num++
+// }
+// while(num<=10);
+//working mechanism of do while loop
+
+//first do while do the task and then check the condition 
+
+let score = 1
+do{
+    console.log(10);
+}
+while(score>=10) // this is not  true statement that's why it run only for one time 
